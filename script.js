5c2ffc66e87e6859a570c419a1ae1935d69f2fb5
tasnimul2 <chowdhury1478@gmail.com>
2021-01-23 13:38:35 -0500

    top portion done

---
+++ script.js
@@ -0,0 +1,35 @@
+/* 
+Guess a number between 1 and 100.
+
+The program will generate a secret number which will be hidden. Players need to guess the number by typing the number 
+into a box (input element).
+
+Initial score is 0. High Score is 0.
+
+1. When there is no input, and user press check button, there is message showing no input.
+
+2. When user put a wrong number, and user press check button, message shows it is too low or too high. 
+There is a score starting from 10 and will be decreasing by 1 every time when user guessing a wrong number.
+
+3. When user put a correct number, background color changes, the secret number shows, background with of 
+secret number changes, and high score will be updated or remained the same.
+
+Implement a game rest functionality, so that the player can make a new guess!
+
+1. Select the element with the 'Play' class and attach a click event handler
+
+2. In the handler function, restore initial values of the 'score' and 'secretNumber' variables
+
+3. When user put a correct number, background color changes, the secret number shows,
+ background with of secret number changes, and high score will be updated or remained the same.
+
+Implement a game rest functionality, so that the player can make a new guess!
+
+1. Select the element with the 'Play' class and attach a click event handler
+
+2. In the handler function, restore initial values of the 'score' and 'secretNumber' variables
+
+3. Restore the initial conditions of the message, number, score and guess input fields
+
+4. Also restore the original background color and number background width
+*/
