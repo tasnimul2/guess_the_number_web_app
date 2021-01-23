5fe335b2e7e1cf8cc320fdfd456873158abfb1d8
shahidemdad <shahidbinemdad@gmail.com>
2021-01-23 15:24:17 -0500

    gameRange func commented

--- script.js
+++ script.js
@@ -54,23 +54,23 @@ function guessGameDefault() { // The default range is from 0 -> 100
     } while (value != number);
 }
 
-function guessGameRange() { // In case we need to select a range for the game
+// function guessGameRange() { // In case we need to select a range for the game
 
-    let number = Math.trunc(Math.random() * 100);
+//     let number = Math.trunc(Math.random() * 100);
 
-    do {
-        let value = prompt("Guess my number");
+//     do {
+//         let value = prompt("Guess my number");
 
-        if (value < 0 || value >= 100) console.log("Out of range.");
+//         if (value < 0 || value >= 100) console.log("Out of range.");
 
-        if (value > number)
-            console.log("Too high!");
-        else if (value < number)
-            console.log("Too low!");
-        else
-            console.log("You found my number!");
-    } while (value != number && min >= max);
-}
+//         if (value > number)
+//             console.log("Too high!");
+//         else if (value < number)
+//             console.log("Too low!");
+//         else
+//             console.log("You found my number!");
+//     } while (value != number && min >= max);
+// }
 
 function isEmpty(value){ // I just wrote it
     return value === "";
